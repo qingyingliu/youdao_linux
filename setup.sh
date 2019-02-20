eac5a3f6188a5c5c2667cdd5519990bc4bb25964
Lewin671 <2596736318@qq.com>
2019-02-20 22:20:43 +0800

    analysis database changed

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ cp -r ./../youdao_linux ~
 # 判断是否成功
 if [ $? -eq 0 ]
 then
-    echo "move the project to ~"
+    echo "move the project to ~ ok!"
 else
     echo "error"
     exit 1
@@ -25,6 +25,16 @@ echo "DATABASE_SCHEMA = '$home/youdao_linux/data/schema.sql'" >> ~/youdao_linux/
 echo "#　默认英式读音，可以通过设置ＳＯＵＮＤ为'UK'来改成美式读音" >> ~/youdao_linux/package/config.py
 echo "# 如果不想要读音，可以注释掉SOUND变量" >> ~/youdao_linux/package/config.py
 echo "SOUND = 'UK'" >> ~/youdao_linux/package/config.py
+echo "write config.py is ok!"
+
+# 安装pygame
+pip install pygame
+if [ $? -eq 0]
+then
+    echo "pygame install is ok!"
+else
+    echo "pygame install is error"
+fi
 
 
 echo "youdao_linux has been installed!"
